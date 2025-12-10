e26a7f67e9043ac4f953e0cbb49b93680e60d265
Alexander Vorobyev <alexander.vorobyev@booking.com>
2025-12-10 11:04:48 +0100

    Fix main container

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <div className="main">
         <Woordjes />
-      </main>
+      </div>
 
     </div>
   )
