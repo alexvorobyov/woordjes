02ab9d3fbcd1890e1e3307a9050599e07500f782
Alexander Vorobyev <alexander.vorobyev@booking.com>
2026-01-14 09:54:53 +0100

    Parser + new words

---
+++ parser/parser.mjs
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from "fs";
+
+const extractorRegexp = /(.*)\(([^\)]+)\)\: (.*)/;
+const content = readFileSync("words.txt", "utf8").split("\n");
+const parsedData = {};
+
+for (const line of content) {
+  if (line.match(extractorRegexp)) {
+    const [, dutchWord, translation, example] = extractorRegexp.exec(line);
+    if (dutchWord && translation && example) {
+      parsedData[dutchWord.trim()] = {
+        translation: translation.trim(),
+        example: example.trim(),
+      };
+    }
+  }
+}
+
+writeFileSync('../woordjes/public/data.json', JSON.stringify(parsedData, null, 2));
